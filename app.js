5bd540e82aa75cbd6c25e2fdc6318e79f3df83fb
mulinater <mrucks@wisc.edu>
2024-10-05 19:39:13 -0500

    added global css & js and edited L2Hunt

---
+++ app.js
@@ -0,0 +1,44 @@
+
+// HEY THERE BUDDY!.... ONLY PLACE CONTENT THAT APPLIES TO ALL PAGES HERE
+
+/* Pro tip, link this app.js to your page in addition to your custom .js file*/
+
+// MOBILE NAVBAR CODE
+// had to add this for fitting navbar better on mobile
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".navbar-burger");
+    const menu = document.getElementById("navbarMenu");
+
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("is-active");
+        menu.classList.toggle("is-active");
+    });
+});
+
+//NAVBAR DISAPPEARING AND REAPPEARING BASED OFF SCROLL
+// ---inspired by Actuarial Club's navbar
+let lastScroll = 0;
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", function () {
+  const currentScroll =
+    window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScroll > lastScroll && currentScroll > 100) {
+    // Scroll down
+    navbar.classList.add("hidden");
+  } else {
+    // Scroll up
+    navbar.classList.remove("hidden");
+  }
+
+  lastScroll = currentScroll <= 0 ? 0 : currentScroll;
+});
+
+let login_button = document.querySelector("#login_button");
+login_button.addEventListener("click", () => {
+  signin_modal.classList.add("is-active");
+});
+signin_modalbg.addEventListener("click", () => {
+  signin_modal.classList.remove("is-active");
+});
