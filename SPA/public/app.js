21d496c4747ae7244cc910fa8b77a18396deac95
Alex <alexmiorth@gmail.com>
2024-11-07 21:50:35 -0600

    fixed two duplicate IDs for sign up and sign in modal. Fixed error at line 264 in app.js

--- SPA/public/app.js
+++ SPA/public/app.js
@@ -261,9 +261,17 @@ quick_contact.onclick = function () {
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
+    const targetId = this.getAttribute("href");
+
+    // Check if the href is not just "#"
+    if (targetId !== "#") {
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+    }
   });
 });
 
